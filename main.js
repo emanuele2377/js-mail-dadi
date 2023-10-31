55a93bdc596c798dace868e4f341fe44123cbd20
emanuele2377 <emanuelekushi7@gmail.com>
2023-10-31 16:35:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const inserisciEmail = prompt("Inserisci la tua email");
+const email = [ "emanuelekushi@gmail.com", "alessandrograndigmail.com", "mariorossi@gmail.com"];
+
+
+for (let i = 0; i < email.length; i++) {
+
+    if (email == email)  {
+        console.log (" valido")
+    }
+
+    if (email == 0)  {
+        console.log ("non valido")
+    }
+    
+
+}
