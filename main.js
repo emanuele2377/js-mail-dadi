3621196db2dc6c9f4f2a7bfc5ac70bce9585349b
emanuele2377 <emanuelekushi7@gmail.com>
2023-11-01 17:32:41 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ for (let i = 0; i < email.length; i++) {
     
     const emailCorrente = email[i];
 
-    if (email == emailCorrente)  {
-
+    if (emailCorrente == inserisciEmail)  {
         console.log (" valido")
     }
-
-   
     
+    else {
+        console.log ("non valido")
+        document.getElementById("container").innerHTML = "utente non autorizzato";
+    }
+    
+    document.getElementById("container").innerHTML = "utente autorizzato";
 
 }
+
